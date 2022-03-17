bd91ffbc3f2de2d028208f66095d8252e062fb29
JackBecknell <52moosefit@gmail.com>
2022-03-17 15:44:32 -0400

    Fixed get-object function

--- supers/views.py
+++ supers/views.py
@@ -21,5 +21,29 @@ class SuperList(APIView):
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
 class SuperDetail(APIView):
-    def print(self, request, format=None):
-        pass
+    def get_object(self, pk):
+        try:
+            return Super.objects.get(pk=pk)
+        except Super.DoesNotExist:
+            raise Http404
+
+        #the Gets_object function relies on the get function to return a single Product by pk
+    def get(self, request, pk, format=None):
+        super = self.get_object(pk)
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+
+    #Updates single product by pk
+    def put(self, request, pk, format=None):
+        super = self.get_object(pk)
+        serializer = SuperSerializer(super, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #Deletes single Product by pk
+    def delete(self, request, pk, format=None):
+        super = self.get_object(pk)
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
